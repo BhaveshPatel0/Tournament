a4aaeaf64f9516acf85ad720be9f11db8ca66822
BhaveshPatel0 <bhavesh05081998@gmail.com>
2023-10-30 17:34:28 +0530

    TeamMatch RecyclerView Set Not Completed

--- app/src/main/java/com/example/tournamentact/adapter/AdapterSelectTeam.kt
+++ app/src/main/java/com/example/tournamentact/adapter/AdapterSelectTeam.kt
@@ -1,7 +1,7 @@
 package com.example.tournamentact.adapter
 
 import android.content.Context
-import android.content.Intent
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,12 +9,12 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tournamentact.R
-import com.example.tournamentact.activity.TeamMemberAct
 import com.example.tournamentact.database.teamname.DataTeamName
 
 class AdapterSelectTeam(val reqContext: Context, val listOfTeamName: List<DataTeamName>) :
-RecyclerView.Adapter<AdapterSelectTeam.HolderTeamName>() {
+    RecyclerView.Adapter<AdapterSelectTeam.HolderTeamName>() {
 
+     val selectedTeam= arrayListOf<DataTeamName>()
     class HolderTeamName(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val tvTeamName: TextView = itemView.findViewById(R.id.tvTeamName)
         val tvTeamName1: TextView = itemView.findViewById(R.id.tvTeamName1)
@@ -23,8 +23,8 @@ RecyclerView.Adapter<AdapterSelectTeam.HolderTeamName>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         HolderTeamName(
-            LayoutInflater.from(reqContext).inflate(R.layout.item_rcv_team_name, parent, false)
-
+            LayoutInflater.from(reqContext).
+            inflate(R.layout.item_rcv_team_name, parent, false)
         )
 
     override fun getItemCount(): Int {
@@ -32,14 +32,31 @@ RecyclerView.Adapter<AdapterSelectTeam.HolderTeamName>() {
     }
 
     override fun onBindViewHolder(holder: HolderTeamName, position: Int) {
+
+        if (selectedTeam.contains(listOfTeamName[position])){
+            holder.llTeamName.setBackgroundColor(Color.BLUE)
+        }else{
+            holder.llTeamName.setBackgroundColor(Color.WHITE)
+        }
+
         val list = listOfTeamName[position]
         holder.tvTeamName.text = list.teamName
         holder.tvTeamName1.text = list.teamName
         holder.llTeamName.setOnClickListener {
 
-            if (listOfTeamName.size==2){
-
+            if (selectedTeam.size<2){
+                if (selectedTeam.contains(listOfTeamName[position])){
+                    selectedTeam.remove(list)
+                }else{
+                    selectedTeam.add(list)
+                }
+            }else{
+//                if (selectedTeam.contains(listOfTeamName[position])){
+                    selectedTeam.remove(list)
+//                }
             }
+
+            notifyDataSetChanged()
 
         }
     }
